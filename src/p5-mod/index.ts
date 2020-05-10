a38ac8f2c11c23be9f11f9a87efc23b6f52e6692
mushtaq <mushtaq.a@gmail.com>
2020-05-10 13:06:53 +0530

    rename

--- src/p5-mod/index.ts
+++ src/p5-mod/index.ts
@@ -1,7 +1,7 @@
 import p5 from 'p5'
 import { Color } from 'p5'
 
-export const sketch = (s: p5) => {
+export const sketch = (p: p5) => {
   let backgroundColor: Color
   let x = 0
   let y = 0
@@ -9,23 +9,23 @@ export const sketch = (s: p5) => {
   const width = 500
   const height = 500
 
-  s.setup = () => {
-    s.createCanvas(width, height)
-    backgroundColor = s.color(s.random(255), s.random(255), s.random(255))
+  p.setup = () => {
+    p.createCanvas(width, height)
+    backgroundColor = p.color(p.random(255), p.random(255), p.random(255))
 
-    x = s.random(width)
+    x = p.random(width)
     y = height / 2
   }
 
-  s.draw = () => {
-    s.background(backgroundColor)
-    s.fill(s.color(255, 0, 0))
-    s.ellipse(x, y, 100, 100)
+  p.draw = () => {
+    p.background(backgroundColor)
+    p.fill(p.color(255, 0, 0))
+    p.ellipse(x, y, 100, 100)
 
     x = (x + 1) % width
   }
 
-  s.mousePressed = () => {
-    backgroundColor = s.color(s.random(255), s.random(255), s.random(255))
+  p.mousePressed = () => {
+    backgroundColor = p.color(p.random(255), p.random(255), p.random(255))
   }
 }
