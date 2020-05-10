ae95afb40dc733bf828212eea39189545817cc37
mushtaq <mushtaq.a@gmail.com>
2020-05-10 08:37:47 +0530

    init

---
+++ __test__/test1.test.ts
@@ -0,0 +1,48 @@
+import { Color, Path, Point } from 'paper'
+import { Geometry } from 'Geometry'
+
+const pc = require('paper-jsdom-canvas')
+
+test('loads and displays greeting', async () => {
+  pc.setup(new pc.Size(500, 500))
+  const path = new Path()
+  path.strokeColor = new Color('grey')
+
+  const start = new Point(500, 500)
+  path.moveTo(start)
+
+  const smallerSide = 100
+
+  path.lineBy(new Point(0, -smallerSide * Math.sqrt(3)))
+  path.lineBy(new Point(-smallerSide, 0))
+  path.closePath()
+  console.log(path.segments.map(x => x))
+})
+
+test('reflect', async () => {
+  pc.setup(new pc.Size(500, 500))
+  const path = new Path()
+  path.strokeColor = new Color('grey')
+
+  path.add(new Point(3.96, -1.41))
+  path.add(new Point(2.78, -3.47))
+  path.add(new Point(10.08, -3.55))
+  path.closePath()
+
+  let path1 = Geometry.reflect(path, 2)
+  // console.log(path1)
+  console.log(path1.segments.map(x => [x.point.x, x.point.y]))
+})
+
+test('angle', async () => {
+  pc.setup(new pc.Size(500, 500))
+
+  let p0 = new Point(0, 0)
+  let p1 = new Point(10, 10)
+  let p2 = new Point(20, 40)
+
+  console.log(p1.angle)
+  console.log(p2.angle)
+  console.log(p2.subtract(p1).angle)
+  console.log(Geometry.angle(p2, p1))
+})
---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // The root of your source code, typically /src
+  // `<rootDir>` is a token Jest substitutes
+  roots: ['<rootDir>/src', '<rootDir>/__test__'],
+
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+
+  // Runs special logic, such as cleaning up components
+  // when using React Testing Library and adds special
+  // extended assertions to Jest
+  // setupFilesAfterEnv: [
+  //     "@testing-library/react/cleanup-after-each",
+  //     "@testing-library/jest-dom/extend-expect"
+  // ],
+
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+
+  // Module file extensions for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', 'src', '__test__'],
+}
