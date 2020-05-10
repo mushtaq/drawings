48671f54a1c55a32db5e7fedb41a701c9ce7a244
mushtaq <mushtaq.a@gmail.com>
2020-05-10 21:52:43 +0530

    event handling

--- src/paper-mod/paperGeom.ts
+++ src/paper-mod/paperGeom.ts
@@ -1,11 +1,11 @@
 import paper from 'paper'
 import { Path, Point, Size, Color } from 'paper'
-import Utils from 'Utils'
+import Utils, { FrameEvent } from 'Utils'
 
 const setup = () => {
   const canvas = <HTMLCanvasElement>document.getElementById('myCanvas')
-  canvas.height = 300
-  canvas.width = 300
+  canvas.height = 600
+  canvas.width = 600
   paper.setup(canvas)
 }
 
@@ -13,17 +13,26 @@ export const paperGeom = () => {
   setup()
 
   let center = paper.view.center
-  let r = new Path.Circle(center, 300)
+  let r = new Path.Circle(center, 600)
   r.fillColor = new Color('grey')
 
-  let rectCenter = center.add(new Point(100, 0))
-  let rect = new Path.Rectangle(rectCenter, new Size(25, 25))
+  let rectCenter = center.add(new Point(200, 0))
+  let rect = new Path.Rectangle(rectCenter, new Size(50, 50))
   rect.fillColor = new Color('white')
-  rect.onFrame = () => rect.rotate(1, center)
+  setEvents(rect)
 
   Utils.rangeArray(1, 12).forEach(i => {
     let item = <paper.Path.Rectangle>rect.clone()
     item.rotate((360 / 12) * i, center)
-    item.onFrame = () => item.rotate(1, center)
+    setEvents(item)
   })
+}
+
+const setEvents = (r: paper.Path.Rectangle) => {
+  r.onClick = (e: MouseEvent) => {
+    r.fillColor = new Color('red')
+  }
+  r.onFrame = (e: FrameEvent) => {
+    r.rotate(1, paper.view.center)
+  }
 }
